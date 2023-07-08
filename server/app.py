4fab7c563d106a211fbd0f8ab0ad96e4478da36d
dennis-nganga <126565131+dennis-nganga@users.noreply.github.com>
2023-07-08 23:18:15 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -9,10 +9,7 @@ from models import User
 class ClearSession(Resource):
 
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.clear()
         return {}, 204
 
 class Signup(Resource):
@@ -25,19 +22,42 @@ class Signup(Resource):
         )
         db.session.add(user)
         db.session.commit()
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+
+    def post(self):
+        json = request.get_json()
+        username = json['username']
+        password = json['password']
+        user = User.query.filter_by(username=username, password_hash=password).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {}, 401
 
 class Logout(Resource):
-    pass
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
-api.add_resource(Signup, '/signup', endpoint='signup')
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+
+api.add_resource(ClearSession, '/clear')
+api.add_resource(Signup, '/signup')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
